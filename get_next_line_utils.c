5dfe65f513b679e241838b8a096e3bc195468c3a
Arnaud DOHOU <adohou@e1r1p3.clusters.42paris.fr>
2022-08-27 22:38:41 +0200

    27/08

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -18,7 +18,7 @@ char	*ft_strchr(char *s, int c)
 
 	if (!s)
 		return(NULL);
-	if (c == 0)
+	if (c == '\0')
 		return ((char *) s + ft_strlen(s));
 		// return(NULL);
 	i = 0;
@@ -36,24 +36,31 @@ char	*ft_strjoin(char *s1, char *s2)
 	char	*newstr;
 	size_t	lens1;
 	size_t	lens2;
-	int		i;
-	int		j;
+	size_t	i;
+	size_t	j;
 
+	if (!s1)
+	{
+		s1 = (char *)malloc(sizeof(char) * 1);
+		s1[0] = '\0';
+	}
+	if (!s1 || !s2)
+		return (NULL);
 	lens1 = ft_strlen(s1);
 	lens2 = ft_strlen(s2);
-	// if (		store)
 	newstr = malloc(sizeof(char) * (lens1 + lens2 + 1));
 	if (!newstr)
 		return (NULL);
 	i = 0;
 	j = 0;
-	while (lens1--)
-		*(newstr + j++) = *(s1 + i++);
+	if (s1)
+		while (lens1--)
+			*(newstr + j++) = *(s1 + i++);
 	i = 0;
 	while (lens2--)
 		*(newstr + j++) = *(s2 + i++);
 	*(newstr + j) = '\0';
-
+	free(s1);
 	return (newstr);
 }
 
